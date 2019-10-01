6253932d323eccad1a6db750b4235685fc7b55e0
Yo <Marcos>
2019-09-30 22:33:17 -0300

    Create menu

---
+++ app/src/main/java/com/example/myactivity/ActionBarActivity.java
@@ -0,0 +1,51 @@
+package com.example.myactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class ActionBarActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_wifi:
+                Toast.makeText(this,"Wi-fi acionado!",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_sair:
+                finish();
+                return true;
+            case R.id.action_sobre:
+                Toast.makeText(this,"Nada a declarar!",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_bluetooth:
+                Toast.makeText(this,"bluetooth acionado!",Toast.LENGTH_LONG).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_bar);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+    }
+}
